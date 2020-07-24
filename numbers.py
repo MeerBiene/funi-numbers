ef2009cfe394bf74c61aea64fb2681136447747b
MeerBiene <b.buhles@gmail.com>
2020-07-24 23:56:52 +0200

    made stuff more user friendly

--- numbers.py
+++ numbers.py
@@ -1,3 +1,8 @@
-for i in range(0, 1000000000):
-    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0:
+
+range = input("Enter your limit now. This is the number up till the skript will run \nAnswer here: ")
+j = 0
+for i in range(1, int(range)):
+    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0 and i % i == 0:
         print(i)
+        j = j + 1
+print("Amount of funi numbers: ", j)
